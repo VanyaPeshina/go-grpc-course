68caae6ee9992ef27982b26dee76836ff7635414
Vanya Peshina <peshina_vanya@abv.bg>
2022-03-23 22:37:50 +0200

    wip

--- generate.sh
+++ generate.sh
@@ -1,1 +1,5 @@
 protoc greet.proto --go_out=.
+
+protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+protoc calculator/calculatorpb/calculator.proto --go_out=plugins=grpc:.
+protoc blog/blogpb/blog.proto --go_out=plugins=grpc:.
